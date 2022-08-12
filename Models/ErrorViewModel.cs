627d7c7d36e885282e24af710754db0b70384d89
petro1909 <petya-97_97@mail.ru>
2022-08-13 00:14:57 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinksShortening.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Link.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinksShortening.Models
+{
+    public class Link
+    {
+        public int Id { set; get; }
+        public string LongURL { set; get; }
+        public string ShortURL { set; get; }
+        public int Jumps { set; get; }
+
+        public Link() { }
+        public Link(string longURL)
+        {
+            LongURL = longURL;
+        }
+    }
+}
