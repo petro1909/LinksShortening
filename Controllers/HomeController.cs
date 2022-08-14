5e83a6e3423022d44f5985307c58dbac621dbef8
petro1909 <petya-97_97@mail.ru>
2022-08-14 12:09:59 +0300

    Add crud methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,92 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using LinksShortening.Database;
 
 namespace LinksShortening.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppDbContext context)
         {
-            _logger = logger;
+            this.context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            return View(context.Links.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult EditOrCreate(int? id)
+        {
+            //Create
+            if (id == null)
+            {
+                return View();
+            }
+
+            //Edit
+            Link link = context.Links.Find(id);
+            if (link != null)
+            {
+                return View(link);
+            }
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult EditOrCreate(Link link)
         {
-            return View();
+            Link actualLink = context.Links.Find(link.Id);
+            if(actualLink == null)
+            {
+                link.Id = 0;
+                link.Jumps = 0;
+                link.CreationDate = DateTime.Now;
+                context.Links.Add(link);
+            } else
+            {
+                actualLink.LongURL = link.LongURL;
+                actualLink.ShortURL = link.ShortURL;
+                context.Links.Update(actualLink);
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
         }
+
+        //[HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Link link = context.Links.Find(id);
+            if (link != null) 
+            {
+                context.Links.Remove(link);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public string GenerateShortedLink(string longURL)
+        {
+            return $"{longURL.GetHashCode()}";
+        }
+
+        public void AcceptLinkJump(int id)
+        {
+            Link link = context.Links.Find(id);
+            if (link != null)
+            {
+                link.Jumps++;
+                context.Links.Update(link);
+                context.SaveChanges();
+            }
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
