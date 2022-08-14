9e0a98322d4f8305744a997e44fa5c9705a15d50
petro1909 <petya-97_97@mail.ru>
2022-08-14 12:08:01 +0300

    Update model structure

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using LinksShortening.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinksShortening.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Link> Links => Set<Link>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Link>(LinkConfig);
+        }
+
+        private void LinkConfig(EntityTypeBuilder<Link> link)
+        {
+            link.ToTable("Links");
+
+            link.Property(l => l.Id).HasColumnName("Id").HasColumnType("INT");
+            link.HasKey(l => l.Id);
+
+            link.Property(l => l.LongURL).HasColumnName("LongURL").HasColumnType("TEXT");
+            link.Property(l => l.CreationDate).HasColumnName("CreationDate").HasColumnType("DATE");
+            link.Property(l => l.ShortURL).HasColumnName("ShortURL").HasColumnType("TEXT");
+            link.Property(l => l.Jumps).HasColumnName("Jumps").HasColumnType("INT");
+        }
+    }
+}
---
+++ Migrations/20220813132628_InitailMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LinksShortening.Migrations
+{
+    public partial class InitailMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LongURL = table.Column<string>(type: "TEXT", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ShortURL = table.Column<string>(type: "TEXT", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreationDate = table.Column<DateTime>(type: "DATE", nullable: false),
+                    Jumps = table.Column<int>(type: "INT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
